08576536ec10acab2f1dc91da496cb5b74eed0ad
Maria Hirota <mhirota@Marias-MacBook-Air-2.local>
2018-02-18 23:06:39 +0800

    add Job ID and sort by scending order

--- routes/postings.js
+++ routes/postings.js
@@ -4,7 +4,7 @@ var app = express()
 // SHOW LIST OF JOB POSTINGS
 app.get('/', function(req, res, next) {
 	req.getConnection(function(error, conn) {
-		conn.query('SELECT * FROM jobpostings ORDER BY job_id DESC',function(err, rows, fields) {
+		conn.query('SELECT * FROM jobpostings ORDER BY job_id ASC',function(err, rows, fields) {
 			//if(err) throw err
 			if (err) {
 				req.flash('error', err)
