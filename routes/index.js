f6cbc43ec89f86f14565f9e662b6cae7d3ee9d4a
Maria Hirota <mhirota@Marias-MacBook-Air-2.local>
2018-02-20 21:46:38 +0800

    add database setup for initial deployment

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,35 @@ var app = express()
 app.get('/', function(req, res) {
     // render to views/index.ejs template file
     res.render('index', {title: 'My Node.js Application'})
+
+    //Database setup - create tables
+    var jobpostingsDb_init = 'CREATE TABLE IF NOT EXISTS jobpostings('
+	+ 'job_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,'
+	+ 'job_name VARCHAR(40) NOT NULL,'
+	+ 'job_description VARCHAR(100) NOT NULL,'
+	+ 'location VARCHAR(30) NOT NULL'
+	+ ')'
+
+    var applicantsDb_init = 'CREATE TABLE IF NOT EXISTS applicants('
+	+ 'applicant_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, '
+	+ 'first_name VARCHAR(40) NOT NULL, '
+	+ 'last_name VARCHAR(40) NOT NULL,'
+	+ 'email VARCHAR(50),'
+	+ 'phone VARCHAR(20),'
+	+ 'job_id INT UNSIGNED NOT NULL,'
+	+ 'applied_date TIMESTAMP NOT NULL'
+	+ ')'
+
+    req.getConnection(function(error, conn) {
+        conn.query(jobpostingsDb_init, function (err) {
+            if (err) {
+		console.log("error creating DB")
+		throw err
+	    } else {
+		console.log("tables created")
+	    }
+        })
+    })
 })
 
 /** 
