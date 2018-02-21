808ef7da4b7f4c89b7c83b929be2e7a6942873ae
Maria Hirota <mhirota@Marias-MacBook-Air-2.local>
2018-02-21 21:45:35 +0800

    change hostname - test

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
 	database: {
-		host:	  '172.30.176.203', 	// database host '172.30.83.49'
+		host:	  '172.30.83.49', 	// database host '172.30.83.49'
 		user: 	  'mhirota', 		// database username
 		password: 'password', 		// database password
 		port: 	  '3306', 	         // MySQL port
