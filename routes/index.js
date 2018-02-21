83f2d6fef50465593eea795adbcca83487363332
Maria Hirota <mhirota@Marias-MacBook-Air-2.local>
2018-02-21 22:18:02 +0800

    debug2 - show mysql environment variables

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,21 @@ app.get('/', function(req, res) {
     console.log('usrname:' + mysql_usrname)
     console.log('pwd:' + mysql_pwd)
 
+    var mysqlServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+    var	mysqlHost = process.env[mysqlServiceName + '_SERVICE_HOST']
+    var	mysqlPort = process.env[mysqlServiceName + '_SERVICE_PORT']
+    var	mysqlDB = process.env[mysqlServiceName + '_DATABASE']
+    var	mysqlPassword = process.env[mysqlServiceName + '_PASSWORD']
+    var	mysqlUser = process.env[mysqlServiceName + '_USER']
+
+    console.log('_____________________________________')
+    console.log('service name :' + mysqlServiceName)
+    console.log('host:' + mysqlHost)
+    console.log('port:' + mysqlPort)
+    console.log('pwd:' + mysqlPassword)
+    console.log('usrname:' + mysqlUser)
+    console.log('database:' + mysqlDB)
+
     //Database setup - create tables
     var jobpostingsDb_init = 'CREATE TABLE IF NOT EXISTS jobpostings('
 	+ 'job_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,'
