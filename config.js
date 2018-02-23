fbc526ba97b7576c8e826524c4d630760ef74f84
Maria Hirota <mhirota@dhcp-65-165.sin.redhat.com>
2018-02-23 16:33:58 +0800

    change config to use environment variables

--- config.js
+++ config.js
@@ -1,18 +1,21 @@
 var mysqlHost = process.env.MYSQL_SERVICE_HOST,
-    mysqlPort = process.env.MYSQL_SERVICE_PORT
+    mysqlPort = process.env.MYSQL_SERVICE_PORT,
+    mysqlUser = process.env.MYSQL_USER,
+    mysqlPwd = process.env.MYSQL_PASSWORD,
+    mysqlDb = process.env.MYSQL_DATABASE
 
 var config = {
-	database: {
-		host:	   mysqlHost, 	// database host '172.30.83.49'
-		user: 	  'mhirota', 		// database username
-		password: 'password', 		// database password
-		port: 	   mysqlPort, 	         // MySQL port '3306'
-		db: 	  'sampledb' 		// database name
-	},
-	server: {
-		host: '127.0.0.1',
-		port: '3000'
-	}
+    database: {
+	host:	   mysqlHost, 	// database host
+	user: 	   mysqlUser, 	// database username
+	password:  mysqlPwd, 	// database password
+	port: 	   mysqlPort,   // MySQL port
+	db: 	   mysqlDb      // database name
+    },
+    server: {
+	host: '127.0.0.1',
+	port: '3000'
+    }
 }
 
 module.exports = config
